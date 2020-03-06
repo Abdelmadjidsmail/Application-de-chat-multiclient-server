acbe7f408d2fa3e3b34c4aab343806ebcd20633e
Houss3m <30732027+Houss3m@users.noreply.github.com>
2020-03-06 22:43:57 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+***Lachemat Houssam Eddine Othman M1 GSI ***
+
+To run this project correctly you need to compile each class, run ServerClient class and choose Server first.
+Then you can add Your clients.. its limited to 10, in the server address textfield you need to type localHost.
+Type your name, and start chatting.
+If you want to send a private message you need to add '@' at the first of the receiver's name.
---
+++ src/Lachemat2/MultiThreadChatServerSync.java
@@ -0,0 +1,69 @@
+
+
+package Lachemat2;
+
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+/**
+ *
+ * @author Houssam Eddine Othman Lachemat
+ */
+
+// the Server class
+public class MultiThreadChatServerSync {
+   // The server socket.
+  private static ServerSocket serverSocket = null;
+  // The client socket.
+  private static Socket clientSocket = null;
+
+  // This chat server can accept up to maxClientsCount clients' connections.
+  private static final int maxClientsCount = 10;
+  private static final clientThread[] threads = new clientThread[maxClientsCount];
+
+  public static void main(String args[]) {
+
+    // The default port number.
+    int portNumber = 5698;
+    if (args.length < 1) {
+      System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
+          + "Now using port number=" + portNumber);
+    } else {
+      portNumber = Integer.valueOf(args[0]).intValue();
+    }
+
+
+    try {
+      serverSocket = new ServerSocket(portNumber);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
+    /*
+     * Create a client socket connection and pass it to a new client
+     * thread.
+     */
+    while (true) {
+      try {
+        clientSocket = serverSocket.accept();
+        int i = 0;
+        for (i = 0; i < maxClientsCount; i++) {
+          if (threads[i] == null) {
+            (threads[i] = new clientThread(clientSocket, threads)).start();
+            break;
+          }
+        }
+        //if there are more then 10 clients..
+        if (i == maxClientsCount) {
+          PrintStream os = new PrintStream(clientSocket.getOutputStream());
+          os.println("Server too busy. Try later.");
+          os.close();
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println(e);
+      }
+    }
+  }  
+}
